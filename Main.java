8bd1451629532de01c9b0849445839671d6c8822
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-04 11:27:35 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,17 @@
+package sample;
+
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
+public class Main{
     public static void main(String[] args) {
-        launch(args);
+        // TODO Auto-generated method stub
+        Map.create();
     }
+
 }
