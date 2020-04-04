4295a60b2853d1fd1da4a6f0ee1efd88b2ce61fb
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-04 11:26:51 +0200

    Update Position.java

--- Position.java
+++ Position.java
@@ -3,42 +3,45 @@ package Tours;
 import javafx.scene.shape.Circle;
 import javafx.scene.transform.Rotate;
 
-public class Position{
+public class Position {
     private double x;
     private double y;
     private double radius;
     private double angle;
 
-    public Position(double x, double y, double angle){
+    public Position(double x, double y){
         this.x = x;
         this.y = y;
-        this.angle = angle;
+        //this.angle = angle;
+
     }
 
-    public double getX(){
+    public double getX() {
         return x;
     }
+
     public void setX(double x) {
         this.x = x;
     }
-    public double getY(){
+
+    public double getY() {
         return y;
     }
-    public void setY(double y){
+
+    public void setY(double y) {
         this.y = y;
     }
-    public void move(double dx, double dy){
-        setX(x+dx);
-        setY(y+dy);
-    }
 
-    public void getBounds(){
-        return new Circle(x, y, double radius);
+    public double distance(Position P1, Position P2){
+        double P1X = Math.pow(P1.getX(), 2.0); double P2X = Math.pow(P2.getX(), 2.0); double P1Y = Math.pow(P1.getY(), 2.0); double P2Y = Math.pow(P2.getY(), 2.0);
+        return Math.sqrt(Math.pow(P1X+P2X, 2.0)+Math.pow(P1Y+P2Y, 2.0));
     }
-
-    public Rotate rotate(double angle){
-        rotate().setAngle(double angle);
-        rotate().setPivotX(double x);
-        rotate().setPivotY(double y);
-    }
+//    public void getBounds(){
+//        return new Circle(x, y, double radius);
+//    }
+    //public Rotate rotate(double angle){
+    //    rotate().setAngle(double angle);
+    //    rotate().setPivotX(double x);
+    //    rotate().setPivotY(double y);
+    //}
 }
