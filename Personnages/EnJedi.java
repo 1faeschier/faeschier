dfcd617069af73f9657f31298611412c28fdb84d
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-04 11:15:56 +0200

    Update EnJedi.java

--- Personnages/EnJedi.java
+++ Personnages/EnJedi.java
@@ -1,40 +1,91 @@
 package Personnages;
 
-
 import Personnages.Enemies;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class EnJedi implements Enemies {
+public class EnJedi extends Ennemie implements Runnable {
     private int id; //=matricule
     private int power; //résistance aux tirs
     private String description; //pour le différencier à l'écran
     private int health; //vie
-    private int speed;//vitesse du perso
+    private int speed = 15;//vitesse du perso = temps en miliseconde entre deux déplacement de 1 unité
+    private Thread t;
 
 
-    public EnSpeeder(int health, int speed, String description){
-        this.power = 50;
-        this.health = 60;
-        this.speed = 30;
-        this.description = description;
+    public EnJedi(Position positioninit) {
+        position = positioninit;
+        t = new Thread(this);
+        t.start();
     }
-
     public int getHealth() {
         return health;
     }
-
     public int getPower(){
         return power;
     }
 
-    public String getDescription{
+    public String getDescription(){
         return description;
     }
 
-    public void looseHealth(int amount) {
-        if (health > 0) {
-            health -= amount;
-        }
+    public Position getPosition() {
+        return position;
     }
 
-        public void move(){...}
+    public EnJedi createnew() {
+        EnJedi b = new EnJedi(position);
+        return b;
+    }
+
+    public Rectangle getforme() {
+        r.setHeight(20);
+        r.setWidth(20);
+        r.setX(position.getX());
+        r.setY(position.getY());
+        r.setStroke(Color.BLUE);
+        return r;
+    }
+
+    public void move(double dx, double dy) {
+        position.setX(position.getX() + dx);
+        position.setY(position.getY() + dy);
+    }
+
+    public void update() {
+        r.setX(position.getX());
+        r.setY(position.getY());
+    }
+
+    @Override
+    public void run() {
+        while ((getPosition().getX()>100) && (getPosition().getY() == 100) && health > 0) {
+            move(-1, 0);
+            try {
+                Thread.sleep(speed);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        while (getPosition().getY()<550 && health > 0) {
+            move(0, 1);
+            try {
+                Thread.sleep(speed);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+        while ((getPosition().getX()<1000) && (getPosition().getY() == 550 && health > 0)) {
+            move(1, 0);
+            try {
+                Thread.sleep(speed);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
+
