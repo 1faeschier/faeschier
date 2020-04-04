e38ef97a3fdbbe05590c907ebcc057a2c5055eba
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-04 11:08:25 +0200

    Update Enemies.java

--- Personnages/Enemies.java
+++ Personnages/Enemies.java
@@ -1,21 +1,34 @@
 package Personnages;
 
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public interface Enemies {
-
+public class Ennemie {
     private int id; //=matricule
     private int power; //résistance aux tirs
     private String description; //pour le différencier à l'écran
-    private int health; //vie
     private int speed;//vitesse du perso
     private static ArrayList<Coordinate> path;
+    public Position position;
+    public int health = 200;
+    public Rectangle r = new Rectangle();
 
-    public void move(){...}
+
+    public void update(){}
+
+
+    Position getPosition(){
+        return position;
+    }
 
     public void looseHealth(int amount) {
-        if (health > 0) {
-            health -= amount;
+        health -= amount;
+        if (health <= 0){
+            r.setWidth(0);
+            r.setHeight(0);
+            r.setStroke(Color.BROWN);
         }
+        System.out.println(health);
     }
 }
