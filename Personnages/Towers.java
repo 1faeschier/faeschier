a9b07b6111a30822ab186ed965f1610304fd086d
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-04 11:23:59 +0200

    Update Towers.java

--- Personnages/Towers.java
+++ Personnages/Towers.java
@@ -2,19 +2,22 @@ package Personnages;
 
 import Personnages.Entities;
 
-public class Towers extends Entities {
+public class Tower extends Entities implements Runnable {
+    private Position position = new Position(500, 130);
+    private int attackRange = 100;
+    private int attackDamage = 5;
+    private int attackSpeed = 100;
+    private Thread t;
     private int buildCost;
     private int buildTime;
-    private int attackRange;
     private int speedRotation;
-    private int attackSpeed;
-    private int attackDamage;
     private int level;
     private int upgradeCost;
     private int upgradeTime;
     private int sellCost;
+    private Ennemie ennemie;
 
-    public Towers(int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed, int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost){
+    public void Towers(int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed, int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost){
         this.buildCost = buildCost;
         this.buildTime = buildTime;
         this.attackRange = attackRange;
@@ -26,27 +29,46 @@ public class Towers extends Entities {
         this.upgradeCost = upgradeCost;
         this.upgradeTime = upgradeTime;
         this.sellCost = sellCost;
+        t = new Thread(this);
+        t.start();
     }
 
-    public void upgrade(int score){
+    public String upgrade(int score){
+        String res = "";
         if (upgradeCost <= score){
             score -= upgradeCost;
             upgradeCost += 20;
-            damage += 20;
+            attackDamage += 20;
             sellCost += 20;
             attackRange += 10;
             attackSpeed += 5;
             upgradeTime += 50;
+            res = "amérlioratioin réalisé avec succès !";
         }
-        else if{
-            return ("Tu n'as pas les fonds nécessaires, retourne shooter des méchants")
+        else {
+            res =  "Tu n'as pas les fonds nécessaires, retourne shooter des méchants";
         }
+        return res;
     }
 
-    public void upgrade(){
+    public Tower createnew() {
+        Tower b = new Tower();
+        return b;
     }
 
     public void shoot(float x, float y){
+    }
+
+    @Override
+    public void run() {
+            while (position.distance(position, ennemie.getPosition()) < attackRange){
+                ennemie.looseHealth(attackDamage);
+                try {
+                    Thread.sleep(attackSpeed);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
     }
 
 }
